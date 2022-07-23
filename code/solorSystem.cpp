4d26cf912aa4ac988c145dcb92e11848ba0b2a5c
unknown <abodhakeemfabl@gmail.com>
2022-07-24 00:11:53 +0300

    loading texture

--- code/solorSystem.cpp
+++ code/solorSystem.cpp
@@ -2,8 +2,10 @@
 #include <GL/glut.h>
 #include <time.h>
 #include "shader.hpp"
-
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 using namespace std;
 
 int w = 520;
@@ -18,28 +20,29 @@ GLfloat points[] = {
 	-0.5f,  0.5f, 0.0f   // top left 
 };
 
-unsigned int indices[] = {  // note that we start from 0!
-	0, 1, 3,   // first triangle
-	1, 2, 3    // second triangle
-};
 
-//genrate ebo
+
+
 	this code in init() methods if i want to do ebo
-	glGenBuffers(1, &ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	
 	
 */
 
 GLfloat points[] =
 {
-	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+	// positions          // colors           // texture coords
+	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+};
+unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
 };
 
-GLfloat triColor[] = { 0.9, 0.0, 0.2, 1.0 };
+float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+
 
 void reshape(int W, int H)
 {
@@ -48,7 +51,9 @@ void reshape(int W, int H)
 	h = H;
 }
 
-GLuint offset;
+unsigned char* textureData;
+
+GLuint ebo;
 
 void init(void)
 {
@@ -64,6 +69,40 @@ void init(void)
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
+	//genrate ebo
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+	//---------------------------------------------------------------------------------------- loading TEXTURE
+	//Generating a texture
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);// all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+
+	 // set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	textureData = stbi_load("../textures/container.jpg", &width, &height, &nrChannels, 0);
+
+	if (textureData) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		cout << "Faild to load texture" << "\n";
+	}
+	stbi_image_free(textureData);
+	//----------------------------------------------------------------------------------------
 	
 	// Load shaders and use the resulting shader program
 	GLuint program = LoadShaders("./shaders/vshader.glsl", "./shaders/fshader.glsl");
@@ -72,27 +111,29 @@ void init(void)
 	// Initialize the vertex position attribute from the vertex shader
 	GLuint loc = glGetAttribLocation(program, "aPos");
 	glEnableVertexAttribArray(loc);
-	glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE,6 * sizeof(float),(void*)0);
+	glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE,8 * sizeof(float),(void*)0);
 
 	GLuint aColor = glGetAttribLocation(program, "aColor");
 	glEnableVertexAttribArray(aColor);
-	glVertexAttribPointer(aColor, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)) );
+	glVertexAttribPointer(aColor, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)) );
+
+	GLuint aTexCoord = glGetAttribLocation(program, "aTexCoord");
+	glEnableVertexAttribArray(aTexCoord);
+	glVertexAttribPointer(aTexCoord, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	int nrAttributes;
 	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
 	cout << "Maximum nr of vertex attributes supported: " << nrAttributes << "\n";
 
-	offset = glGetUniformLocation(program, "offset");
-
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	
 }
 
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glUniform1f(offset,0.6);
-	glDrawArrays(GL_TRIANGLES, 0, NumPoints);    // draw the points
-	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	//glDrawArrays(GL_TRIANGLES, 0, NumPoints);    // draw the points
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	glutSwapBuffers();
 }
 
@@ -123,5 +164,6 @@ int main(int argc, char** argv)
 	glutReshapeFunc(reshape);
 
 	glutMainLoop();
+	
 	return 0;
 }
