766f7157b2b0eecbddbc9cdfc879ae4642cac5bd
unknown <abodhakeemfabl@gmail.com>
2022-07-23 15:11:38 +0300

    hello tringle

---
+++ code/solorSystem.cpp
@@ -0,0 +1,123 @@
+#include <glew.h>
+#include <GL/glut.h>
+#include <time.h>
+#include "shader.hpp"
+
+#include <iostream>
+using namespace std;
+
+int w = 520;
+int h = 520;
+
+const int NumPoints = 3;
+/*
+GLfloat points[] = {  
+	0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+};
+
+unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+};
+
+//genrate ebo
+	this code in init() methods if i want to do ebo
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	
+*/
+
+GLfloat points[] =
+{
+	-0.5f,-0.5f,0.0,
+	0.5f,-0.5f,0.0,
+	0.0f,0.5f,0.0
+};
+
+GLfloat triColor[] = { 0.9, 0.0, 0.2, 1.0 };
+
+void reshape(int W, int H)
+{
+	glViewport(0, 0, W, H);
+	w = W;
+	h = H;
+}
+
+GLuint uniformColor;
+
+void init(void)
+{
+
+	// Create a vertex array object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// Create and initialize a buffer object
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	
+	// Load shaders and use the resulting shader program
+	GLuint program = LoadShaders("./shaders/vshader.glsl", "./shaders/fshader.glsl");
+	glUseProgram(program);
+
+	// Initialize the vertex position attribute from the vertex shader
+	GLuint loc = glGetAttribLocation(program, "aPos");
+	glEnableVertexAttribArray(loc);
+	glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, 0,0);
+
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	cout << "Maximum nr of vertex attributes supported: " << nrAttributes << "\n";
+
+	uniformColor = glGetUniformLocation(program, "ourColor");
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+}
+
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUniform4fv(uniformColor, 1, triColor);
+	glDrawArrays(GL_TRIANGLES, 0, NumPoints);    // draw the points
+	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glutSwapBuffers();
+}
+
+
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 033:
+		exit(EXIT_SUCCESS);
+		break;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(w, h);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("test");
+
+	glewInit();
+	init();
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutReshapeFunc(reshape);
+
+	glutMainLoop();
+	return 0;
+}
